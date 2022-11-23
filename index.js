d765a4a2d65327074d06315a76a0b13fc90ca4ed
Jakub Palka <jakub.palka97@gmail.comgit config --global user.name Jakub>
2022-11-23 15:29:06 +0100

    Ready popup baner

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ let addBtn //przycisk ADD - dodaje nowe elementy do listy
 let ulList // lista zadań, tagi Ul
 let newTodo //Nowo dodane zadanie
 
+let popup // popup
+let popupInfo // tekst w popupie
+let todoToEdit // edytowany toodo
+let popupInput // iput w popupie
+let popupAddBtn // button w popupie
+let popupCloseBtn // przycisk anuluj w popupie
 
 const main = () => {
     prepareDOMElements()
@@ -15,10 +21,21 @@ const prepareDOMElements = () => {
   errorInfo= document.querySelector('.error-info')
   addBtn= document.querySelector('.btn-add')
   ulList= document.querySelector('.todolist ul')
+
+  popup= document.querySelector('.popup')
+  popupInfo=document.querySelector('.popup-info')
+  popupInput=document.querySelector('.popup-input')
+  popupAddBtn=document.querySelector('.accept')
+  popupCloseBtn=document.querySelector('.cancel')
+
+
 }
 
 const prepareDOMEvents = () => {
   addBtn.addEventListener('click',addNewTask)
+  ulList.addEventListener('click', checkClick)
+  popupCloseBtn.addEventListener('click', closePopup)
+  popupAddBtn.addEventListener('click', changeTodoText)
 }
 
 const addNewTask = () => {
@@ -59,7 +76,34 @@ const createToolsArea = () => {
 
 }
 
+const checkClick = e => {
+  if (e.target.matches('.complete')) {
+      e.target.closest('li').classList.toggle('completed')
+      e.target.classList.toggle('completed')
+  }else if (e.target.matches('.edit')){
+      editTodo(e)
+  }else if (e.target.matches('.delete')){
+      console.log('delete')
+  }
+}
 
+const editTodo = e => {
+    todoToEdit = e.target.closest('li')
+    popupInput.value = todoToEdit.firstChild.textContent
+    popup.style.display='flex'
+}
+const closePopup = () => {
+  popup.style.display='none'
+  popupInfo.textContent = ''
+}
+const changeTodoText = () => {
+  if (popupInput.value !== ''){
+      todoToEdit.firstChild.textContent = popupInput.value
+      popup.style.display='none'
+  }else {
+      popupInfo.textContent = 'Wpisz treść zadania !'
+  }
+}
 
 
 
